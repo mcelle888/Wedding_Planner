5009b8b13548a51cea8b35fab562b165d7703d16
mcelle888 <14894@coderacademy.edu.au>
2023-10-23 21:34:22 +1100

    added more colour

--- src/weddingcost.py
+++ src/weddingcost.py
@@ -1,13 +1,19 @@
+from colorama import Fore, Back, Style
 class WeddingItems:
     name = None
     cost = None
     def __init__(self,item_type):
         self.item_type = item_type
     def ask_user(self):
-        confirm = input(f"Have you chosen a {self.item_type}? Please enter 'y' or 'n': ")
+        confirm = input(Fore.RED + f"Have you chosen a {self.item_type}? Please enter 'y' or 'n' ")
+        print(Style.RESET_ALL)
+        # confirm = input(f"Have you chosen a {self.item_type}? Please enter 'y' or 'n': ")
         if confirm == "y":
             self.name = input(f"Please enter your {self.item_type} name: ") 
-            self.cost = input(f"Please enter your {self.item_type} cost:")
+            self.cost = input(f"Please enter your {self.item_type} cost: ")
+        
+           
+            
         elif confirm == "n":
             self.get_recc()
     # def get_recc():
@@ -24,8 +30,7 @@ class Venue(WeddingItems):
 
 venue_object = Venue("venue")
 venue_object.ask_user()
-print(venue_object.__dict__)
- 
+print(Fore.BLUE + f"{venue_object.__dict__}")
 
 # FLORIST
 class Florist(WeddingItems):
@@ -34,7 +39,7 @@ class Florist(WeddingItems):
 
 florist_object = Florist("florist")
 florist_object.ask_user()
-print(florist_object.__dict__) 
+print(Fore.LIGHTBLUE_EX + f"{florist_object.__dict__}") 
 
 # CATERING
 class Food(WeddingItems):
@@ -43,7 +48,7 @@ class Food(WeddingItems):
 
 food_object = Food("caterer")
 food_object.ask_user()
-print(food_object.__dict__)
+print(Fore.LIGHTBLUE_EX + f"{food_object.__dict__}")
 
 # DECOR
 class Decoration(WeddingItems):
@@ -52,7 +57,7 @@ class Decoration(WeddingItems):
 
 decoration_object = Decoration("event decorater ")
 decoration_object.ask_user()
-print(decoration_object.__dict__)
+print(Fore.LIGHTBLUE_EX + f"{decoration_object.__dict}")
 
 # BEAUTY SERVICES
 class MakeupHair(WeddingItems):
@@ -61,7 +66,7 @@ class MakeupHair(WeddingItems):
 
 makeup_hair_object = MakeupHair("make up and hair stylist")
 makeup_hair_object.ask_user()
-print(makeup_hair_object.__dict__)
+print(Fore.LIGHTBLUE_EX + f"{makeup_hair_object.__dict__}")
 
 # PHOTO AND VIDEO
 class PhotoVideo(WeddingItems):
@@ -69,7 +74,7 @@ class PhotoVideo(WeddingItems):
         print("Here are some recommendations for you ")
 photo_video_object = PhotoVideo("photographer and videographer ")
 photo_video_object.ask_user()
-print(photo_video_object.__dict__)
+print(Fore.LIGHTBLUE_EX + f"{photo_video_object.__dict}")
 
 # CLOTHES
 class Dress(WeddingItems):
@@ -77,12 +82,11 @@ class Dress(WeddingItems):
         print("Here are some recommendations for you ")
 dress_object = Dress("dress and suit merchant ")
 dress_object.ask_user()
-print(dress_object.__dict__)
-
+print(Fore.LIGHTBLUE_EX + f"{dress_object.__dict__}")
 # CAKE
 class Cake(WeddingItems): 
     def get_recc(self):
         print("Here are some recommendations for you")
 cake_object = Cake("cake shop ")
 cake_object.ask_user()
-print(cake_object.__dict__)
+print(Fore.LIGHTBLUE_EX + f"{cake_object.__dict__}")
