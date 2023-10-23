b2a570f9bf2b1aa2247bd6d846edd4fb47aeefcc
mcelle888 <14894@coderacademy.edu.au>
2023-10-23 16:02:44 +1100

    feature 1 started

---
+++ src/main.py
@@ -0,0 +1,39 @@
+from pyfiglet import Figlet
+import emoji
+from colorama import Fore, Back, Style
+import datetime 
+
+# Opening App
+f = Figlet(font='contessa')
+print(f.renderText('Congratulations!'))
+print(emoji.emojize("Congrats on your engagement :red_heart:  Let's get you started!", variant="emoji_type"))
+
+# Basic information from user input
+def introduction(): 
+    user_name = input("Please enter your name: ")
+    wedding_date_y = int(input("Please enter the year of the wedding (yyyy): "))
+    wedding_date_m = int(input("Please enter the month of the wedding (mm): "))
+    wedding_date_d = int(input("Please enter the day of the wedding (dd): "))
+    # calculates total days till wedding
+    today_date = datetime.date.today()
+    wedding_date = datetime.date(wedding_date_y, wedding_date_m, wedding_date_d)
+    diff = (wedding_date-today_date).days
+   
+    total_budget= int(input("Enter your budget: "))
+    print(Fore.RED + "Name:", user_name, "\nDate:", wedding_date_d , wedding_date_m , wedding_date_y ,"\nTotal Budget:", total_budget)
+    print(Style.RESET_ALL)
+    # Checks with user if inputs are correct
+    confirm = input("Are these details correct? Please type 'y' or 'n': ")
+    if confirm == "n":
+        print("Okay, let's try again")
+        introduction() 
+    elif confirm == "y": 
+        print(emoji.emojize("Thanks for that:grinning_face_with_smiling_eyes: ", variant="emoji_type"))
+        print("There are", diff, "days left till the wedding. Let's get planning!")
+      
+introduction()
+
+
+
+
+ 
